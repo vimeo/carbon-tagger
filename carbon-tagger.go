29a5d8a6de7503dd54b516177326cc572bebe03e
Dieter Plaetinck <dieter@vimeo.com>
2013-06-17 18:40:33 -0400

    decouple ingest, metric tracking, and forwarding work
    * forward asap, don't wait for sql
    * control over # sql workers (for now only 1),
      instead of 1 mysql connection for every input connection

--- carbon-tagger.go
+++ carbon-tagger.go
@@ -18,6 +18,11 @@ func dieIfError(err error) {
 	}
 }
 
+type metricSpec struct {
+	metric_id string
+	tags      map[string]string
+}
+
 func main() {
 	var (
 		mysql_user              = config.String("mysql.user", "carbon_tagger")
@@ -42,14 +47,7 @@ func main() {
 	defer db.Close()
 	// Open doesn't open a connection. Validate DSN data:
 	err = db.Ping()
-	dieIfError(err)
-	statement_insert_tag, err := db.Prepare("INSERT INTO tags (tag_key, tag_val) VALUES( ?, ? )")
-	dieIfError(err)
-	statement_select_tag, err := db.Prepare("SELECT tag_id FROM tags WHERE tag_key=? AND tag_val=?")
-	dieIfError(err)
-	statement_insert_metric, err := db.Prepare("INSERT INTO metrics VALUES( ? )")
-	dieIfError(err)
-	statement_insert_link, err := db.Prepare("INSERT INTO metrics_tags VALUES( ?, ? )")
+	db.SetMaxIdleConns(80)
 	dieIfError(err)
 
 	// listen for incoming metrics
@@ -66,129 +64,149 @@ func main() {
 	conn_out, err := net.Dial("tcp", out)
 	dieIfError(err)
 
+	packets_to_forward := make(chan []byte)
+	// 1 forwarding worker should suffice?
+	go forwardPackets(conn_out, packets_to_forward)
+
+	metrics_to_track := make(chan metricSpec)
+	// for now, only 1 sql persist worker...
+	go trackMetrics(db, metrics_to_track)
+
 	for {
 		fmt.Println("ready to accept")
 		conn_in, err := listener.Accept()
-		fmt.Println("accepted")
 		dieIfError(err)
 		fmt.Println("handling connection in subroutine")
-		go handleClient(conn_in, conn_out, db, statement_insert_tag, statement_select_tag, statement_insert_metric, statement_insert_link)
+		go handleClient(conn_in, metrics_to_track, packets_to_forward)
 	}
 }
 
-func parseTagBasedMetric(metric string) (metric_id string, tags map[string]string, err error) {
-	fmt.Printf(">incoming: %s\n", metric)
+func parseTagBasedMetric(metric_line string) (metric metricSpec, err error) {
 	// metric_spec value unix_timestamp
-	elements := strings.Split(metric, " ")
-	metric_id = ""
+	elements := strings.Split(metric_line, " ")
+	metric_id := ""
 	if len(elements) != 3 {
-		return metric_id, nil, errors.New("metric doesn't contain exactly 3 nodes")
+		return metricSpec{metric_id, nil}, errors.New("metric doesn't contain exactly 3 nodes")
 	}
 	metric_id = elements[0]
 	nodes := strings.Split(metric_id, ".")
-	tags = make(map[string]string)
+	tags := make(map[string]string)
 	// TODO make sure incoming tags are sorted
 	for _, node := range nodes {
 		tag := strings.Split(node, "=")
 		if len(tag) != 2 {
-			return metric_id, nil, errors.New("bad metric spec: each node must be a 'tag_k=tag_v' pair")
+			return metricSpec{metric_id, nil}, errors.New("bad metric spec: each node must be a 'tag_k=tag_v' pair")
 		}
 		if tag[0] == "" || tag[1] == "" {
-			return metric_id, nil, errors.New("bad metric spec: tag_k and tag_v must be non-empty strings")
+			return metricSpec{metric_id, nil}, errors.New("bad metric spec: tag_k and tag_v must be non-empty strings")
 		}
 
 		tags[tag[0]] = tag[1]
 	}
 	if _, ok := tags["unit"]; !ok {
-		return metric_id, nil, errors.New("bad metric spec: unit tag (mandatory) not specified")
+		return metricSpec{metric_id, nil}, errors.New("bad metric spec: unit tag (mandatory) not specified")
 	}
 	if len(tags) < 2 {
-		return metric_id, nil, errors.New("bad metric spec: must have at least one tag_k/tag_v pair beyond unit")
+		return metricSpec{metric_id, nil}, errors.New("bad metric spec: must have at least one tag_k/tag_v pair beyond unit")
 	}
-	return
+	return metricSpec{metric_id, tags}, nil
 }
 
-func forwardPacket(conn_out net.Conn, b []byte) {
-	fmt.Println("be4 conn out write", string(b))
-	_, err := conn_out.Write(b)
-	fmt.Println("after conn out write")
-	dieIfError(err) // todo something more sensible
+func trackMetrics(db *sql.DB, metrics_to_track chan metricSpec) {
+	statement_insert_tag, err := db.Prepare("INSERT INTO tags (tag_key, tag_val) VALUES( ?, ? )")
+	dieIfError(err)
+	statement_select_tag, err := db.Prepare("SELECT tag_id FROM tags WHERE tag_key=? AND tag_val=?")
+	dieIfError(err)
+	statement_insert_metric, err := db.Prepare("INSERT INTO metrics VALUES( ? )")
+	dieIfError(err)
+	statement_insert_link, err := db.Prepare("INSERT INTO metrics_tags VALUES( ?, ? )")
+	dieIfError(err)
+	for {
+		metric := <-metrics_to_track
+		// TODO this should go in a transaction. for now we first store all tag_k=tag_v pairs (if they are orphans, it's not so bad)
+		// then add the metric, than the coupling between metric and tags. <-- all this should def. be in a transaction
+		tag_ids := make([]int64, 0) //maybe set cap to len(tags) or so
+		for tag_k, tag_v := range metric.tags {
+			res, err := statement_insert_tag.Exec(tag_k, tag_v)
+			if err != nil {
+				if err.(*mysql.MySQLError).Number == 1062 { // Error 1062: Duplicate entry
+					var id int64
+					err := statement_select_tag.QueryRow(tag_k, tag_v).Scan(&id)
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "Can't lookup the id of tag %s=%s: %s\n", tag_k, tag_v, err.Error())
+						return
+					}
+					tag_ids = append(tag_ids, id)
+				} else {
+					fmt.Fprintf(os.Stderr, "can't store tag %s=%s: %s\n", tag_k, tag_v, err.Error())
+					return
+				}
+			} else {
+				id, err := res.LastInsertId()
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "can't get id for just inserted tag %s=%s: %s\n", tag_k, tag_v, err.Error())
+					return
+				} else {
+					tag_ids = append(tag_ids, id)
+				}
+			}
+		}
+		_, err = statement_insert_metric.Exec(metric.metric_id)
+		if err != nil {
+			if err.(*mysql.MySQLError).Number != 1062 { // Error 1062: Duplicate entry 'unit=f.b=aeu' for key 'PRIMARY'
+				fmt.Fprintf(os.Stderr, "can't store metric %s:%s\n", metric.metric_id, err.Error())
+				return
+			}
+		} else {
+			// the metric is newly inserted.. we still have to couple it to the tags.
+			// so we assume if the metric already existed, we don't need to do this anymore. which is not very accurate since we don't use transactions yet
+			for _, tag_id := range tag_ids {
+				_, err = statement_insert_link.Exec(metric.metric_id, tag_id)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "can't link metric %s to tag:%s\n", metric.metric_id, err.Error())
+					return
+				}
+			}
+		}
+	}
+	// TODO should i close the mysql connection here? i always get lots of stale sockets to mysql :(
 }
 
-func handleClient(conn_in net.Conn, conn_out net.Conn, db *sql.DB, statement_insert_tag *sql.Stmt, statement_select_tag *sql.Stmt, statement_insert_metric *sql.Stmt, statement_insert_link *sql.Stmt) {
-	//    defer conn_in.Close()
-	fmt.Println("handleClient begin")
+func forwardPackets(conn_out net.Conn, packets_to_forward chan []byte) {
+	for {
+		packet := <-packets_to_forward
+		fmt.Println("forwarding packet.....maybe..")
+		_, err := conn_out.Write(packet)
+		dieIfError(err) // todo something more sensible
+		fmt.Println("packet forwarded successfully")
+	}
+}
+
+func handleClient(conn_in net.Conn, metrics_to_track chan metricSpec, packets_to_forward chan []byte) {
+	defer fmt.Println("handleClient ending..")
+	defer conn_in.Close()
 	var buf [512]byte
 	for {
-		fmt.Println("ready to read")
+		//TODO how will this handle multiple lines on input?
 		bytes, err := conn_in.Read(buf[0:])
-		fmt.Println("read data")
-		fmt.Println(string(buf[0:]), bytes, err)
 		if err != nil {
+			fmt.Printf("error reading from incoming connection")
 			return
 		}
 		str := string(buf[0:bytes])
 		if strings.ContainsAny(str, "=") {
 			str = strings.TrimSpace(str)
-			metric_id, tags, err := parseTagBasedMetric(str)
+			metric, err := parseTagBasedMetric(str)
 			if err != nil {
 				fmt.Printf("DEBUG: invalid tag based metric, ignoring (%s)\n", err)
 			} else {
 				fmt.Printf("DEBUG: valid tag based metric %s, storing tags and forwarding\n", strings.TrimSpace(str))
-				// TODO this should go in a transaction. for now we first store all tag_k=tag_v pairs (if they are orphans, it's not so bad)
-				// then add the metric, than the coupling between metric and tags. <-- all this should def. be in a transaction
-				tag_ids := make([]int64, 0) //maybe set cap to len(tags) or so
-				for tag_k, tag_v := range tags {
-					fmt.Println("Key:", tag_k, "Value:", tag_v)
-					res, err := statement_insert_tag.Exec(tag_k, tag_v)
-					if err != nil {
-						if err.(*mysql.MySQLError).Number == 1062 { // Error 1062: Duplicate entry
-							var id int64
-							err := statement_select_tag.QueryRow(tag_k, tag_v).Scan(&id)
-							if err != nil {
-								fmt.Fprintf(os.Stderr, "Can't lookup the id of tag %s=%s: %s\n", tag_k, tag_v, err.Error())
-								return
-							}
-							tag_ids = append(tag_ids, id)
-						} else {
-							fmt.Fprintf(os.Stderr, "can't store tag %s=%s: %s\n", tag_k, tag_v, err.Error())
-							return
-						}
-					} else {
-						id, err := res.LastInsertId()
-						if err != nil {
-							fmt.Fprintf(os.Stderr, "can't get id for just inserted tag %s=%s: %s\n", tag_k, tag_v, err.Error())
-							return
-						} else {
-							tag_ids = append(tag_ids, id)
-						}
-					}
-				}
-				_, err = statement_insert_metric.Exec(metric_id)
-				if err != nil {
-					if err.(*mysql.MySQLError).Number != 1062 { // Error 1062: Duplicate entry 'unit=f.b=aeu' for key 'PRIMARY'
-						fmt.Fprintf(os.Stderr, "can't store metric %s:%s\n", metric_id, err.Error())
-						return
-					}
-				} else {
-					// the metric is newly inserted.. we still have to couple it to the tags.
-					// so we assume if the metric already existed, we don't need to do this anymore. which is not very accurate since we don't use transactions yet
-					for _, tag_id := range tag_ids {
-						_, err = statement_insert_link.Exec(metric_id, tag_id)
-						if err != nil {
-							fmt.Fprintf(os.Stderr, "can't link metric %s to tag:%s\n", metric_id, err.Error())
-							return
-						}
-					}
-				}
-				forwardPacket(conn_out, buf[0:bytes])
+				metrics_to_track <- metric
+				packets_to_forward <- buf[0:bytes]
 			}
 		} else {
 			fmt.Printf("DEBUG: not tag based, forwarding metric %s\n", strings.TrimSpace(str))
-			forwardPacket(conn_out, buf[0:bytes])
+			packets_to_forward <- buf[0:bytes]
 		}
 	}
-	fmt.Println("handleClient ending.. closing conn_in")
-	conn_in.Close()
-	fmt.Println("handleClient ended")
 }
