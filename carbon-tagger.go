905e4480c3242bb1602ef2882fe8205136bfeab5
Dieter Plaetinck <dieter@vimeo.com>
2015-03-17 17:28:22 -0400

    make code easier to read

--- carbon-tagger.go
+++ carbon-tagger.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"errors"
 	"flag"
 	"fmt"
 	"github.com/Dieterbe/go-metrics"
@@ -127,46 +126,6 @@ func main() {
 	}
 }
 
-func parseTagBasedMetric(metric_line string) (metric metricSpec, err error) {
-	// metric_spec value unix_timestamp
-	elements := strings.Split(metric_line, " ")
-	metric_id := ""
-	if len(elements) != 3 {
-		return metricSpec{metric_id, nil}, errors.New(fmt.Sprintf("metric doesn't contain exactly 3 nodes: %s", metric_line))
-	}
-	metric_id = elements[0]
-	nodes := strings.Split(metric_id, ".")
-	tags := make(map[string]string)
-	for i, node := range nodes {
-		var tag []string
-		if strings.Contains(node, "_is_") {
-			tag = strings.Split(node, "_is_")
-		} else {
-			tag = strings.Split(node, "=")
-		}
-		if len(tag) > 2 {
-			return metricSpec{metric_id, nil}, errors.New("bad metric spec: more than 1 equals")
-		} else if len(tag) < 2 {
-			tags[fmt.Sprintf("n%d", i+1)] = node
-		} else if tag[0] == "" || tag[1] == "" {
-			return metricSpec{metric_id, nil}, errors.New("bad metric spec: tag_k and tag_v must be non-empty strings")
-		} else {
-			// k=v format, and both are != ""
-			tags[tag[0]] = tag[1]
-		}
-	}
-	if u, ok := tags["unit"]; !ok {
-		return metricSpec{metric_id, nil}, errors.New("bad metric spec: unit tag (mandatory) not specified")
-	} else if strings.HasSuffix(u, "ps") {
-		tags["unit"] = u[:len(u)-2] + "/s"
-	}
-
-	if len(tags) < 2 {
-		return metricSpec{metric_id, nil}, errors.New("bad metric spec: must have at least one tag_k/tag_v pair beyond unit")
-	}
-	return metricSpec{metric_id, tags}, nil
-}
-
 func handleClient(conn_in net.Conn) {
 	in_conns_current.Inc(1)
 	defer in_conns_current.Dec(1)
@@ -198,12 +157,12 @@ func processInputLines() {
 		str := string(buf)
 		if bytes.Contains(buf, equals1) || bytes.Contains(buf, equals2) {
 			str = strings.TrimSpace(str)
-			metric, err := parseTagBasedMetric(str)
+			metric, err := NewMetricSpec(str)
 			if err != nil {
 				in_metrics_proto2_bad_total.Inc(1)
 			} else {
 				in_metrics_proto2_good_total.Inc(1)
-				proto2_read <- metric
+				proto2_read <- *metric
 			}
 		} else {
 			elements := strings.Split(str, " ")
--- metrics.go
+++ metrics.go
@@ -1,11 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
 
 type metricSpec struct {
 	metric_id string
 	tags      map[string]string
 }
+
+func NewMetricSpec(metric_line string) (metric *metricSpec, err error) {
+	// metric_spec value unix_timestamp
+	elements := strings.Split(metric_line, " ")
+	metric_id := ""
+	if len(elements) != 3 {
+		return nil, errors.New(fmt.Sprintf("metric doesn't contain exactly 3 nodes: %s", metric_line))
+	}
+	metric_id = elements[0]
+	nodes := strings.Split(metric_id, ".")
+	tags := make(map[string]string)
+	for i, node := range nodes {
+		var tag []string
+		if strings.Contains(node, "_is_") {
+			tag = strings.Split(node, "_is_")
+		} else {
+			tag = strings.Split(node, "=")
+		}
+		if len(tag) > 2 {
+			return nil, errors.New("bad metric spec: more than 1 equals")
+		} else if len(tag) < 2 {
+			tags[fmt.Sprintf("n%d", i+1)] = node
+		} else if tag[0] == "" || tag[1] == "" {
+			return nil, errors.New("bad metric spec: tag_k and tag_v must be non-empty strings")
+		} else {
+			// k=v format, and both are != ""
+			tags[tag[0]] = tag[1]
+		}
+	}
+	if u, ok := tags["unit"]; !ok {
+		return nil, errors.New("bad metric spec: unit tag (mandatory) not specified")
+	} else if strings.HasSuffix(u, "ps") {
+		tags["unit"] = u[:len(u)-2] + "/s"
+	}
+
+	if len(tags) < 2 {
+		return nil, errors.New("bad metric spec: must have at least one tag_k/tag_v pair beyond unit")
+	}
+	return &metricSpec{metric_id, tags}, nil
+}
+
 type MetricEs struct {
 	Tags []string `json:"tags"`
 }
